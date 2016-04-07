c4698506955ad372181d0665900481a21e160567
安诺爱成长 <1399487511@qq.com>
2016-04-07 20:34:21 +0800

    android线程池demo

---
+++ app/src/main/java/com/example/annuoaichengzhang/androiddevelopartdemo/OneActivity.java
@@ -0,0 +1,57 @@
+package com.example.annuoaichengzhang.androiddevelopartdemo;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class OneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        // 线程池demo
+        Runnable command = new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(2000);
+            }
+        };
+
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(4);
+        fixedThreadPool.execute(command);
+
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        cachedThreadPool.execute(command);
+
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(4);
+        scheduledThreadPool.schedule(command, 2000, TimeUnit.MILLISECONDS);
+        scheduledThreadPool.scheduleAtFixedRate(command, 10, 1000, TimeUnit.MILLISECONDS);
+
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        singleThreadExecutor.execute(command);
+    }
+
+}
